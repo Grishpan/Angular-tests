27caf81c6a9d8d4c2ba4618aeaf7c03c6ae79330
Gregory Rogovin <gregoryr@realcommerce.co.il>
2019-12-12 09:34:03 +0200

    Fixed subscription

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {RxjsDebounceComponent} from './rxjs-debounce/rxjs-debounce.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'rxjs-debounce', component: RxjsDebounceComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
